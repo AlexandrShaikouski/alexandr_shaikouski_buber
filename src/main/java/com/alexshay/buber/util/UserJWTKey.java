5545e8264ce82a7ccb2cd2db6c1f6e23122679ca
Alex <sash_shay@mail.ru>
2019-03-07 19:23:49 +0300

    change JWT

--- src/main/java/com/alexshay/buber/util/UserJWTKey.java
+++ src/main/java/com/alexshay/buber/util/UserJWTKey.java
@@ -5,6 +5,8 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
@@ -15,6 +17,7 @@ import java.util.concurrent.locks.ReentrantLock;
 
 
 public class UserJWTKey {
+    private static final Logger LOGGER = LogManager.getLogger(UserJWTKey.class);
     private static UserJWTKey instance;
     private static Lock lock = new ReentrantLock();
     private static String SECRET_KEY = "oeRaYY7Wo24sDqKSX3IM9ASGmdGPmkTd9jo1QTy4b7P9Ze5_9hKolVX8xNrQDcNRfVEdTZNOuOyqEGhXEbdJI-ZQ19k_o9MI0y3eZN2lp9jow55FfXMiINEdt1XR85VipRLSOkT6kSpzs2x-jbLDiz9iFVzkd81YKxMgPA7VfZeQUm4n-mOmnWMaVX30zGFU4L3oPBctYKkl4dYfqYWqRNfrgPJVi5DGFjywgxx0ASEiJHtV72paI3fDR2XwlSkyhhmY-ICjCRmsJN4fX1pdoL8a18-aQrvyu4j0Os6dVPYIoPvvY0SAZtWYKHfM15g7A3HD4cVREf9cUsprCRK93w";
@@ -41,16 +44,17 @@ public class UserJWTKey {
         SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 
         Date now = new Date();
-
+        LOGGER.info("get byte");
         byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(SECRET_KEY);
+        LOGGER.info("get key");
         Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
-
+        LOGGER.info("build jwt");
         JwtBuilder builder = Jwts.builder().setId(id)
                 .setIssuedAt(now)
                 .setSubject(subject)
                 .setIssuer(issuer)
                 .signWith(signingKey);
-
+        LOGGER.info("compact jwt");
         String jwt = builder.compact();
         return jwt;
     }
