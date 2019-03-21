eade9b46cae2aecab591d5095cef2de33bdf9176
Alex <sash_shay@mail.ru>
2019-03-21 16:45:51 +0300

    add authorization filter for driver

---
+++ src/main/java/com/alexshay/buber/filter/AuthorizationDriverFilter.java
@@ -0,0 +1,45 @@
+package com.alexshay.buber.filter;
+
+import com.alexshay.buber.domain.Role;
+import com.alexshay.buber.domain.User;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(dispatcherTypes = {
+        DispatcherType.REQUEST,
+        DispatcherType.FORWARD,
+        DispatcherType.INCLUDE
+}, urlPatterns = "/WEB-INF/jsp/driver/*",
+        initParams = {@WebInitParam(name = "INDEX_PATH", value = "/index.jsp")})
+public class AuthorizationDriverFilter implements Filter {
+    private String indexPath;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        indexPath = filterConfig.getInitParameter("INDEX_PATH");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+
+        HttpSession session = httpServletRequest.getSession();
+        User user = (User) session.getAttribute("user");
+        if (!user.getRole().equals(Role.DRIVER)) {
+            httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + indexPath);
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
